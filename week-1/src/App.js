8637056dbe24f34bade044c23e49b5780ea56d88
WeepingHeron <kimtaehoon_98@naver.com>
2023-02-25 02:06:46 +0900

    research 추가

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
-function  Son() {
-  return <div>나는 아들이에요!</div>;
+// props를 통해 부모 -> 자식 데이터가 전달됐다.
+function  Son(props) {
+  console.log('props', props.motherName, props.grandfatherName)
+  return <div>나는 {props.motherName}의 아들, {props.grandfatherName}의 손자에요!</div>;
 }
 
-function Mother() {
+// 부모 -> 자식 정보를 전달했다!
+function Mother(props) {
   const name = '흥부인';
-  return <Son />;
+  const name1 = props.grandfatherName;
+  console.log('props', props.grandfatherName)
+  return <Son motherName = {name} grandfatherName = {name1} />;
 }
 
 function Grandfather() {
-  return <Mother />;
+  const name1 = '흥부버지';
+  return <Mother grandfatherName = {name1} />;
 }
 
 function App() {
