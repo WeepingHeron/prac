c12d9b58c7746d7e0225187491622a655b7690ac
WeepingHeron <kimtaehoon_98@naver.com>
2023-02-25 23:30:46 +0900

    css 분리 추가

---
+++ style-project/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app-style">
+      <div className="component-style">감자</div>
+      <div className="component-style">고구마</div>
+      <div className="component-style">오이</div>
+      <div className="component-style">가지</div>
+      <div className="component-style">옥수수</div>
+    </div>
+  );
+};
+
+export default App;
